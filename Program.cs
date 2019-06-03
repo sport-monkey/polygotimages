230b1c4aec6bff03cf06ea49e2b136c01890cf72
sport-monkey <ed.gough@gmail.com>
2019-06-03 15:46:46 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using SixLabors.ImageSharp;
+
+namespace ImageSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            using (var image = Image.Load($"Image.jpg"))
+            {
+                Console.WriteLine(2);
+                using (var fp = File.OpenWrite($"Images{Path.DirectorySeparatorChar}Image_New.jpg"))
+                {
+                    Console.WriteLine(3);
+                    image.SaveAsJpeg(fp);
+                    Console.WriteLine(4);
+                }
+            }
+        }
+    }
+}
